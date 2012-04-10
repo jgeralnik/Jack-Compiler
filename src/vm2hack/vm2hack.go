192398224479130fa4d543e0e8bbdc0b606b96d8
Joey Geralnik <jgeralnik@gmail.com>
2012-04-10 13:54:51 +0300

    Finished Exercize 1
    Implemented various push and pop operators.
    Includes local, argument, this, that, temp, pointer and static

--- src/vm2hack/vm2hack.go
+++ src/vm2hack/vm2hack.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
+	"strconv"
 	"strings"
 )
 
@@ -16,9 +18,19 @@ var (
 		"and": "&",
 		"or":  "|",
 	}
+	segments = map[string]string{
+		"local":    "LCL",
+		"argument": "ARG",
+		"this":     "THIS",
+		"that":     "THAT",
+	}
+	bases = map[string]int{
+		"temp":    5,
+		"pointer": 3,
+	}
 )
 
-func ConvertLine(line string) (result string) {
+func ConvertLine(line string, filename string) (result string) {
 	words := strings.Fields(line)
 	if len(words) == 0 || strings.HasPrefix(words[0], "//") {
 		return
@@ -26,7 +38,9 @@ func ConvertLine(line string) (result string) {
 
 	switch words[0] {
 	case "push":
-		result = push(words[1:])
+		result = push(words[1:], filename)
+	case "pop":
+		result = pop(words[1:], filename)
 	case "add", "sub", "and", "or":
 		result = fmt.Sprintf("@SP\nA=M-1\nD=M\n@SP\nM=M-1\nA=M-1\nM=M%sD\n", operators[words[0]])
 	case "eq", "gt", "lt":
@@ -40,12 +54,40 @@ func ConvertLine(line string) (result string) {
 	return
 }
 
-func push(words []string) (result string) {
+func push(words []string, filename string) (result string) {
 	switch words[0] {
 	case "constant":
 		result = fmt.Sprintf("@%s\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n", words[1])
+	case "local", "argument", "this", "that":
+		result = fmt.Sprintf("@%s\nD=M\n@%s\nA=D+A\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n", segments[words[0]], words[1])
+	case "temp", "pointer":
+		offset, err := strconv.Atoi(words[1])
+		if err != nil {
+			panic(fmt.Sprintf("Non number passed to %s", words[0]))
+		}
+		result = fmt.Sprintf("@%d\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n", offset+bases[words[0]])
+	case "static":
+		result = fmt.Sprintf("@%s.%s\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n", filename, words[1])
 	default:
-		panic("Invalid push")
+		panic(fmt.Sprintf("Invalid push: %s", words[0]))
+	}
+	return
+}
+
+func pop(words []string, filename string) (result string) {
+	switch words[0] {
+	case "local", "argument", "this", "that":
+		result = fmt.Sprintf("@%s\nD=M\n@%s\nD=D+A\n@SP\nM=M-1\nA=M\nD=D+M\nA=D-M\nD=D-A\nM=D\n", segments[words[0]], words[1])
+	case "temp", "pointer":
+		offset, err := strconv.Atoi(words[1])
+		if err != nil {
+			panic(fmt.Sprintf("Non number passed to %s", words[0]))
+		}
+		result = fmt.Sprintf("@SP\nM=M-1\nA=M\nD=M\n@%d\nM=D\n", offset+bases[words[0]])
+	case "static":
+		result = fmt.Sprintf("@SP\nM=M-1\nA=M\nD=M\n@%s.%s\nM=D\n", filename, words[1])
+	default:
+		panic(fmt.Sprintf("Invalid pop: %s", words[0]))
 	}
 	return
 }
@@ -64,6 +106,9 @@ func ConvertFile(inputfile string, outputfile string) error {
 	}
 	defer output.Close()
 
+	filename := filepath.Base(inputfile)
+	filename = filename[:len(filename)-3] //Remove ".vm" from filename
+
 	for {
 		line, _, err := reader.ReadLine()
 		if err != nil {
@@ -72,7 +117,7 @@ func ConvertFile(inputfile string, outputfile string) error {
 			}
 			return err
 		}
-		output.WriteString(ConvertLine(string(line)))
+		output.WriteString(ConvertLine(string(line), filename))
 	}
 	return nil
 }
