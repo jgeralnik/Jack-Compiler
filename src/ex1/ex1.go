7734d7ac0299ff6ddeeadd7321661c870fc91ad0
Joey Geralnik <jgeralnik@gmail.com>
2012-05-06 00:46:31 +0300

    Added default output file

--- src/ex1/ex1.go
+++ src/ex1/ex1.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"os"
+	"path/filepath"
 	"vm2hack"
 )
 
 func main() {
-	vm2hack.ConvertFile(os.Args[1], os.Args[2])
+	if len(os.Args) > 2 {
+		vm2hack.ConvertFile(os.Args[1], os.Args[2])
+	} else {
+		base := filepath.Base(os.Args[1][:len(os.Args[1])-len(filepath.Ext(os.Args[1]))])
+		vm2hack.ConvertFile(os.Args[1], filepath.Join(filepath.Dir(os.Args[1]), base+".asm"))
+	}
 }
