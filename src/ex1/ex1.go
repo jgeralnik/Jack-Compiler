6fc173b5ebe2e4b0f159cb2ef8ba7a31d744d7d0
Joey Geralnik <jgeralnik@gmail.com>
2012-05-06 02:58:12 +0300

    Finished targil 2
    Implemented various program control operators, as well as function
    calls and returns. Also created bootstrap code and allowed for
    compilation of entire directory.

--- src/ex1/ex1.go
+++ src/ex1/ex1.go
@@ -7,10 +7,24 @@ import (
 )
 
 func main() {
-	if len(os.Args) > 2 {
-		vm2hack.ConvertFile(os.Args[1], os.Args[2])
+	inputInfo, err := os.Stat(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	if inputInfo.IsDir() {
+		if len(os.Args) > 2 {
+			vm2hack.ConvertDirectory(os.Args[1], os.Args[2])
+		} else {
+			base := filepath.Base(os.Args[1])
+			vm2hack.ConvertDirectory(os.Args[1], filepath.Join(os.Args[1], base+".asm"))
+		}
 	} else {
-		base := filepath.Base(os.Args[1][:len(os.Args[1])-len(filepath.Ext(os.Args[1]))])
-		vm2hack.ConvertFile(os.Args[1], filepath.Join(filepath.Dir(os.Args[1]), base+".asm"))
+		if len(os.Args) > 2 {
+			vm2hack.ConvertFile(os.Args[1], os.Args[2])
+		} else {
+			dir, file := filepath.Split(os.Args[1])
+			base := file[:len(file)-len(filepath.Ext(file))]
+			vm2hack.ConvertFile(os.Args[1], filepath.Join(dir, base+".asm"))
+		}
 	}
 }
--- src/vm2hack/vm2hack.go
+++ src/vm2hack/vm2hack.go
@@ -28,6 +28,7 @@ var (
 		"temp":    5,
 		"pointer": 3,
 	}
+	function string
 )
 
 func ConvertLine(line string, filename string) (result string) {
@@ -48,6 +49,42 @@ func ConvertLine(line string, filename string) (result string) {
 		label += 2
 	case "neg":
 		result = "@SP\nA=M-1\nM=-M\n"
+	case "not":
+		result = "@SP\nA=M-1\nM=!M\n"
+	case "label":
+		result = fmt.Sprintf("(%s$%s)\n", function, words[1])
+	case "goto":
+		result = fmt.Sprintf("@%s$%s\n0;JMP\n", function, words[1])
+	case "if-goto":
+		result = fmt.Sprintf("@SP\nM=M-1\nA=M\nD=M\n@%s$%s\nD;JNE\n", function, words[1])
+	case "function":
+		result = fmt.Sprintf("(%s)\n@SP\nA=M\n", words[1])
+		k, _ := strconv.Atoi(words[2])
+		for i := 0; i < k; i++ {
+			result += "M=0\nA=A+1\n"
+		}
+		result += "D=A\n@SP\nM=D\n"
+	case "call":
+		result = fmt.Sprintf("@LABEL%d\nD=A\n@SP\nM=M+1\nA=M-1\nM=D\n", label) //Push return address
+		segments := []string{"LCL", "ARG", "THIS", "THAT"}
+		for _, segment := range segments {
+			result += fmt.Sprintf("@%s\nD=M\n@SP\nM=M+1\nA=M-1\nM=D\n", segment) //Push LCL,ARG,THIS,THAT
+		}
+		n, _ := strconv.Atoi(words[2])
+		result += fmt.Sprintf("@SP\nD=M\n@%d\nD=D-A\n@ARG\nM=D\n", n+5)   //ARG = SP-n-5
+		result += "@SP\nD=M\n@LCL\nM=D\n"                                 //LCL=SP
+		result += fmt.Sprintf("@%s\n0;JMP\n(LABEL%d)\n", words[1], label) //Jump to function
+		label += 1
+	case "return":
+		result = "@LCL\nD=M\n@13\nM=D\n"                   //Store frame in temp
+		result += "@5\nA=D-A\nD=M\n@14\nM=D\n"             //Store RET in temp
+		result += "@SP\nM=M-1\nA=M\nD=M\n@ARG\nA=M\nM=D\n" //*ARG = pop()
+		result += "@ARG\nD=M+1\n@SP\nM=D\n"                //*SP = ARG+1
+		segments := []string{"THAT", "THIS", "ARG", "LCL"}
+		for _, segment := range segments {
+			result += fmt.Sprintf("@13\nM=M-1\nA=M\nD=M\n@%s\nM=D\n", segment) //Pop THAT,THIS,ARG,LCL
+		}
+		result += "@14\nA=M\n0;JMP\n" //Goto ret
 	default:
 		panic(fmt.Sprintf("Unknown1 command %s", words[0]))
 	}
@@ -120,4 +157,48 @@ func ConvertFile(inputfile string, outputfile string) error {
 		output.WriteString(ConvertLine(string(line), filename))
 	}
 	return nil
+}
+
+func walk(outputfile *os.File) filepath.WalkFunc {
+	return func(inputfile string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if filepath.Ext(inputfile) == ".vm" {
+			file, err := os.Open(inputfile)
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			reader := bufio.NewReader(file)
+
+			filename := filepath.Base(inputfile)
+			filename = filename[:len(filename)-3] //Remove ".vm" from filename
+
+			for {
+				line, _, err := reader.ReadLine()
+				if err != nil {
+					if err == io.EOF {
+						break
+					}
+					return err
+				}
+				outputfile.WriteString(ConvertLine(string(line), filename))
+			}
+		}
+		return nil
+	}
+}
+
+func ConvertDirectory(directory string, outputfile string) error {
+	file, err := os.Create(outputfile)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	file.WriteString("@256\nD=A\n@SP\nM=D\n")
+	file.WriteString(ConvertLine("call Sys.init 0", "system"))
+	return filepath.Walk(directory, walk(file))
 }
